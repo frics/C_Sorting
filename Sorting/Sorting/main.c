efd4388c1ced2ccbbba7257bfe68a628f01f3984
Frics <jimmy9711@naver.com>
2020-05-25 01:56:33 +0900

    Add Basic files

---
+++ Sorting/Sorting/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sorting
+//
+//  Created by James Choi on 2020/05/25.
+//  Copyright © 2020 James Choi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
